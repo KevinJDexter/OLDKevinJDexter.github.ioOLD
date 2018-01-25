8b66dfcbba21142832b3e7b13df4baf55edad1f9
KevinJDexter <kevin.j.dexter@gmail.com>
2018-01-25 11:43:53 -0600

    Button text change attempt 3

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 function toggleMintahoeDetails() {
   if (document.getElementById("mintahoe-details").style.display == "none") {
     document.getElementById("mintahoe-details").style.display = "block";
-    document.getElementById("mintahoe-button").text = "Less...";
+    document.getElementById("mintahoe-button").textContent = "Less...";
   } else {
     document.getElementById("mintahoe-details").style.display = "none";
-    document.getElementById("mintahoe-button").text = "More...";
+    document.getElementById("mintahoe-button").textContent = "More...";
   };
 };
 
 function toggleProtolabsDetails() {
   if (document.getElementById("protolabs-details").style.display == "none") {
     document.getElementById("protolabs-details").style.display = "block";
-    document.getElementById("protolabs-button").text = "Less...";
+    document.getElementById("protolabs-button").textContent = "Less...";
   } else {
     document.getElementById("protolabs-details").style.display = "none";
-    document.getElementById("protolabs-button").text = "More...";
+    document.getElementById("protolabs-button").textContent = "More...";
   };
 };
 
 function toggleTutorDetails() {
   if (document.getElementById("tutor-details").style.display == "none") {
     document.getElementById("tutor-details").style.display = "block";
-    document.getElementById("tutor-button").text = "Less...";
+    document.getElementById("tutor-button").textContent = "Less...";
   } else {
     document.getElementById("tutor-details").style.display = "none";
-    document.getElementById("tutor-button").text = "More...";
+    document.getElementById("tutor-button").textContent = "More...";
   };
 };
 
 function toggleVideoDetails() {
   if (document.getElementById("video-details").style.display == "none") {
     document.getElementById("video-details").style.display = "block";
-    document.getElementById("video-button").text = "Less...";
+    document.getElementById("video-button").textContent = "Less...";
   } else {
     document.getElementById("video-details").style.display = "none";
-    document.getElementById("video-button").text = "More...";
+    document.getElementById("video-button").textContent = "More...";
   };
 };
