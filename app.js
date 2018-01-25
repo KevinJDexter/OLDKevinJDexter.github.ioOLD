7438dc1b6ac7238758224c33ff15ce016c0009e0
KevinJDexter <kevin.j.dexter@gmail.com>
2018-01-25 10:57:02 -0600

    Javascript initial groundwork

---
+++ app.js
@@ -0,0 +1,27 @@
+function displayMintahoeDetails() {
+  if (document.getElementById("mintahoe-details").style.display == "none") {
+    document.getElementById("mintahoe-details").style.display = "block";
+  } else {
+    document.getElementById("mintahoe-details").style.display = "none";
+};
+
+function displayProtolabsDetails() {
+  if (document.getElementById("protolabs-details").style.display == "none") {
+    document.getElementById("protolabs-details").style.display = "block";
+  } else {
+    document.getElementById("protolabs-details").style.display = "none";
+};
+
+function displayTutorDetails() {
+  if (document.getElementById("tutor-details").style.display == "none") {
+    document.getElementById("tutor-details").style.display = "block";
+  } else {
+    document.getElementById("tutor-details").style.display = "none";
+};
+
+function displayVideoDetails() {
+  if (document.getElementById("video-details").style.display == "none") {
+    document.getElementById("video-details").style.display = "block";
+  } else {
+    document.getElementById("video-details").style.display = "none";
+};
